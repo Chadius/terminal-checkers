f06710173fd51d98126226b4276e74e1d5def427
Chad Serrant <chad_serrant@hms.harvard.edu>
2018-07-13 16:05:19 -0400

    master: Starting to draw board.

--- terminal-checkers.py
+++ terminal-checkers.py
@@ -1,3 +1,19 @@
+class CheckerboardDisplay(object):
+    """Actually displays the checkerboard.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(**kwargs)
+
+    def draw_board(self):
+        """Draw the given board.
+        """
+        print ("Insert drawing here")
+        # Rows start from 8 on white's side, 1 on the black's side
+        # Columns start from a on the left, g on the right
+
 if __name__ == '__main__':
-    keyboard_input = input("What is this?")
-    print ("I'm just echoing what you said: {echo}".format(echo=keyboard_input))
+    #keyboard_input = input("What is this?")
+    #print ("I'm just echoing what you said: {echo}".format(echo=keyboard_input))
+
+    checkerboard_display = CheckerboardDisplay()
+    checkerboard_display.draw_board()
